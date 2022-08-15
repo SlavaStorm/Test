9e0d88b8ff2093e009aef7399ca60dc754445f08
Илья <ankud1nov@yandex.ru>
2022-08-15 22:46:55 +0400

    Названия колонок

--- 123/Tabel.cs
+++ 123/Tabel.cs
@@ -1,11 +1,18 @@
-﻿namespace TabelDoc.Models
+﻿using System.ComponentModel;
+
+namespace TabelDoc.Models
 {
     public class Tabel
-    {      
+    {
+        [DisplayName("№")]
         public int Number { get; set; }
+        [DisplayName("ФИО")]
         public string FullName { get; set; }
+        [DisplayName("Невыполненых РКК")]
         public int UnfulfilledDoc { get; set; }
+        [DisplayName("Невыполненых обращений")]
         public int Unfulfilledletter { get; set; }
+        [DisplayName("Общее количество")]
         public int TotalNumber { get; set; }
 
         public  Tabel()
